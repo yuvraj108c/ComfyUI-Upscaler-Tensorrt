bdad3d789c1cc9e80b6f42e27870d2f64d050f82
yuvraj108c <yuvraj108c@gmail.com>
2024-09-24 21:34:59 +0400

    cache model + fix resize bug

--- __init__.py
+++ __init__.py
@@ -6,13 +6,14 @@ import cv2
 from comfy.utils import ProgressBar
 from .utilities import Engine
 
-ENGINE_DIR = os.path.join(folder_paths.models_dir,"tensorrt","upscaler")
+ENGINE_DIR = os.path.join(folder_paths.models_dir, "tensorrt", "upscaler")
+
 
 class UpscalerTensorrt:
     @classmethod
     def INPUT_TYPES(s):
         return {
-            "required": { 
+            "required": {
                 "images": ("IMAGE",),
                 "engine": (os.listdir(ENGINE_DIR),),
                 "resize_to": (["none", "HD", "FHD", "2k", "4k"],),
@@ -30,25 +31,25 @@ class UpscalerTensorrt:
 
         match resize_to:
             case "HD":
-                final_width=1280
-                final_height=720
+                final_width = 1280
+                final_height = 720
             case "FHD":
-                final_width=1920
-                final_height=1080
+                final_width = 1920
+                final_height = 1080
             case "2k":
-                final_width=2560
-                final_height=1440
+                final_width = 2560
+                final_height = 1440
             case "4k":
                 final_width = 3840
                 final_height = 2160
-            case _:
+            case "none":
                 final_width = width*4
                 final_height = height*4
 
         if aspect_ratio == 1.0:
             final_width = final_height
 
-        if aspect_ratio < 1.0:
+        if aspect_ratio < 1.0 and resize_to != "none":
             temp = final_width
             final_width = final_height
             final_height = temp
@@ -56,46 +57,56 @@ class UpscalerTensorrt:
         return (final_width, final_height)
 
     def main(self, images, engine, resize_to):
-        images = images.permute(0, 3, 1, 2) # B,C,W,H
-        B,C,W,H = images.shape
-        shape_dict = {
-            "input": {"shape": (1, 3, W, H)},
-            "output": {"shape": (1, 3, W*4, H*4)},
-        }
-        # setup tensorrt engine
-        engine = Engine(os.path.join(ENGINE_DIR,engine))
-        engine.load()
-        engine.activate()
-        engine.allocate_buffers(shape_dict=shape_dict)
-        cudaStream = torch.cuda.current_stream().cuda_stream
+        images_bchw = images.permute(0, 3, 1, 2)
+        B, C, H, W = images_bchw.shape
 
+        shape_dict = {
+            "input": {"shape": (1, 3, H, W)},
+            "output": {"shape": (1, 3, H*4, W*4)},
+        }
+
+        # setup tensorrt engine
+        engine_path = os.path.join(ENGINE_DIR, engine)
+        if (not hasattr(self, 'engine') or self.engine_label != engine):
+            self.engine = Engine(engine_path)
+            self.engine.load()
+            self.engine.activate()
+            self.engine_label = engine
+        else:
+            print(f"Using cached TensorRT engine: {engine_path}")
+
+        # dynamic memory allocation, can't be cached
+        self.engine.allocate_buffers(shape_dict=shape_dict)
+
+        cudaStream = torch.cuda.current_stream().cuda_stream
         pbar = ProgressBar(B)
-        images_list = list(torch.split(images, split_size_or_sections=1))
+        images_list = list(torch.split(images_bchw, split_size_or_sections=1))
 
         upscaled_frames = []
-        final_width, final_height = self.get_final_resolutions(W,H,resize_to)
+        final_width, final_height = self.get_final_resolutions(W, H, resize_to)
 
         for img in images_list:
-            result = engine.infer({"input": img},cudaStream)
+            result = self.engine.infer({"input": img}, cudaStream)
 
             output = result['output'].cpu().numpy().squeeze(0)
             output = np.transpose(output, (1, 2, 0))
             output = np.clip(255.0 * output, 0, 255).astype(np.uint8)
-            output = cv2.resize(output, (final_height, final_width))
+            output = cv2.resize(output, (final_width, final_height))
 
             upscaled_frames.append(output)
             pbar.update(1)
-        
-        upscaled_frames_np = np.array(upscaled_frames).astype(np.float32) / 255.0
+
+        upscaled_frames_np = np.array(
+            upscaled_frames).astype(np.float32) / 255.0
         return (torch.from_numpy(upscaled_frames_np),)
 
 
-NODE_CLASS_MAPPINGS = { 
-    "UpscalerTensorrt" : UpscalerTensorrt,
+NODE_CLASS_MAPPINGS = {
+    "UpscalerTensorrt": UpscalerTensorrt,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-     "UpscalerTensorrt" : "Upscaler Tensorrt",
+    "UpscalerTensorrt": "Upscaler Tensorrt",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
